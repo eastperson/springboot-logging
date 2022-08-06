d189e289ad56f00f1e28be7e9fa1c857aef6e527
EP <kjuioqq8@gmail.com>
2022-08-06 20:04:38 +0900

    test: 테스트 추가

---
+++ src/test/kotlin/com/ep/springbootlogging/SpringbootLoggingApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.ep.springbootlogging
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootLoggingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
